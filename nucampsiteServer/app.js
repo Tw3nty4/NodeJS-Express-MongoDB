91d45e9c1fb7cbaf6cd97a1e6947d1dec63b7121
Keith Brubaker <keithbrubaker0@gmail.com>
2022-03-03 13:23:24 -0500

    Init

---
+++ nucampsiteServer/app.js
@@ -0,0 +1,82 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const config = require('./config');
+const authenticate = require('./authenticate');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
+
+const mongoose = require('mongoose');
+const e = require('express');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log(
+  'Connected correctly to server'), 
+  err => console.log(err)
+  );
+
+const app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+      return next();
+    } else {
+        console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+        res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    }
+});
+
+// engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
